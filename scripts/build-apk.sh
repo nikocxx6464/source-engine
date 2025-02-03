08642318e6420ad32cb75f2334289eb4decafc70
vl <vladchudov636@gmail.com>
2025-02-04 02:08:09 +0500

    Update Scrips

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -2,10 +2,10 @@ export ICON=ez1.png
 export PACKAGE=EntropyZero
 export APP_NAME="Entropy Zero"
 cd srceng-mod-launcher
-git clone https://gitlab.com/LostGamer/android-sdk
+git clone https://github.com/ItzVladik/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
 git pull
-chmod +x android/script.sh
+chmod +x android/scripts/script.sh
 ./android/scripts/script.sh
 chmod +x waf
 ./waf configure -T release --javac-source-target=8 &&
