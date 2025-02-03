e91a6b2279459961c6dde32e3ab313c0f009125a
vl <vladchudov636@gmail.com>
2025-02-04 01:25:05 +0500

    Update

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -2,10 +2,10 @@ export ICON=lc.png
 export PACKAGE=lostcoast
 export APP_NAME="Half-Life 2: Lost Coast"
 cd srceng-mod-launcher
-git clone https://gitlab.com/LostGamer/android-sdk
+git clone https://github.com/ItzVladik/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
 git pull
-chmod +x android/script.sh
+chmod +x android/scripts/script.sh
 ./android/scripts/script.sh
 chmod +x waf
 ./waf configure -T release --javac-source-target=8 &&
